a096920d980eb05d659b0d2ec3abf69e834e4f18
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-09-08 09:43:25 -0500

    Pokemon Sprites Demo

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-for (let i = 0; i < 100; i++) {
-    const btn = document.createElement('button');
-    btn.innerText = 'Click';
-    container.appendChild(btn);
+const baseURL = 'http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+
+const container = document.querySelector('#container');
+
+
+for (let i = 1; i <= 151; i++) {
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon')
+
+    const label = document.createElement('span');
+    label.innerText = `#${i}`;
+
+    const newImg = document.createElement('img');
+    newImg.src = `${baseURL}${i}.png`
+
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon)
 }
