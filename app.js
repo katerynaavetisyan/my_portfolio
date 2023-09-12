ef627b42c10cba28fcff01630afc06be222799b1
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-09-12 12:51:13 -0500

    onmouseenter property

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ btn.onclick = function () {
     console.log("You clicked me!");
     console.log("I hope it worked!");
 }
+
+
+
+function scream() {
+    console.log("AAAAAAAAAAHHH");
+    console.log("Stop touching me!");
+}
+btn.onmouseenter = scream;
