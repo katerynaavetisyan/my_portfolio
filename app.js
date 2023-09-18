7ee0fce8f5e2139c11dd470b3107a438f2a83b4c
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-09-18 12:17:44 -0500

    Click Events Exercise

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-function talk() {
-    console.log('Talking');
-}
-function shout() {
-    console.log('SHOUT');
-}
-const tasButton = document.querySelector('#tas');
-
-tasButton.addEventListener('click', talk)
-
-
-tasButton.addEventListener('click', shout)
+const bh = document.querySelector('#hello');
+bh.addEventListener('click', function () {
+    console.log("hello")
+});
+const bye = document.querySelector('#goodbye');
+bye.addEventListener('click', function () {
+    console.log("goodbye")
+})
 
