ecee32f6a75b50516b0e033185563b0a3e0e6433
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-09-19 15:43:50 -0500

    clicked added

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const button = document.querySelector('button');
-const h1 = document.querySelector('h1');
 
-button.addEventListener('click', function () {
-    const newColor = makeRandColor()
-    document.body.style.backgroundColor = newColor;
-    h1.innerText = newColor;
-})
 
 const makeRandColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
     return `rgb(${r},${g},${b})`;
+}
+
+const buttons = document.querySelectorAll('button');
+
+for (let button of buttons) {
+    button.addEventListener('click', function () {
+        console.log('CLICKED');
+    })
 }
