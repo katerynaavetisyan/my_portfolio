bf52a84241021927f166e90f0ebe97b08cb308f7
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-09-19 15:56:11 -0500

    added keyword this

--- app.js
+++ app.js
@@ -10,16 +10,15 @@ const makeRandColor = () => {
 const buttons = document.querySelectorAll('button');
 
 for (let button of buttons) {
-    button.addEventListener('click', function () {
-        button.style.backgroundColor = makeRandColor();
-        button.style.color = makeRandColor();
-    })
+    button.addEventListener('click', colorize)
 }
 
 const h1s = document.querySelectorAll('h1');
 for (let h1 of h1s) {
-    h1.addEventListener('click', function () {
-        h1.style.backgroundColor = makeRandColor();
-        h1.style.color = makeRandColor();
-    })
+    h1.addEventListener('click', colorize)
+}
+
+function colorize() {
+    this.style.backgroundColor = makeRandColor();
+    this.style.color = makeRandColor();
 }
