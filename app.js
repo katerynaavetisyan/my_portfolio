a2d4fc0ba7395af3b37254a5e7ac6790edfb1ff3
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-09-19 14:54:36 -0500

    Random Color Exercise

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const button = document.querySelector('button');
+const h1 = document.querySelector('h1');
 button.addEventListener('click', function () {
-    console.log('Test');
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const newColor = `rgb(${r},${g},${b})`;
+    document.body.style.backgroundColor = newColor;
+    h1.innerText = newColor;
 })
