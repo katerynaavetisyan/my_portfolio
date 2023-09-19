0ff5b0be5d72ff25b5c5d74c4bec1139967d7e7a
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-09-19 15:52:25 -0500

    made random color text and background

--- app.js
+++ app.js
@@ -12,5 +12,14 @@ const buttons = document.querySelectorAll('button');
 for (let button of buttons) {
     button.addEventListener('click', function () {
         button.style.backgroundColor = makeRandColor();
+        button.style.color = makeRandColor();
+    })
+}
+
+const h1s = document.querySelectorAll('h1');
+for (let h1 of h1s) {
+    h1.addEventListener('click', function () {
+        h1.style.backgroundColor = makeRandColor();
+        h1.style.color = makeRandColor();
     })
 }
