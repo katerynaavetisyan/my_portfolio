204c0454762704455da957aca84f3a9e098a4486
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-09-20 09:31:26 -0500

    color practice

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ for (let button of buttons) {
     button.addEventListener('click', colorize)
 }
 
+
 const h1s = document.querySelectorAll('h1');
 for (let h1 of h1s) {
     h1.addEventListener('click', colorize)
 }
+
 
 function colorize() {
     this.style.backgroundColor = makeRandColor();
