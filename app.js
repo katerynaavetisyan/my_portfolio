c8172b68b5e556f7eb21ed1c0b5215d8e512853a
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-09-21 12:38:41 -0500

    keydown

--- app.js
+++ app.js
@@ -2,3 +2,10 @@
 document.querySelector('button').addEventListener('click', function (evt) {
     console.log(evt);
 })
+
+
+const input = document.querySelector('input');
+
+input.addEventListener('keydown', function () {
+    console.log("KEYDOWN");
+})
