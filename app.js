b9761e05e7c04f617ca38cf6a6ecdecf049228b6
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-09-26 16:36:08 -0500

    e code

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.querySelector('button').addEventListener('click', function (evt) {
 const input = document.querySelector('input');
 
 
+
 input.addEventListener('keydown', function (e) {
     console.log(e.key);
     console.log(e.code);
