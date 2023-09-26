8be3d1b2ea04dcf6e437428dbab1345e4b68fd64
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-09-26 11:44:36 -0500

    KEYUP AND KEYDOWN

--- app.js
+++ app.js
@@ -3,18 +3,14 @@ document.querySelector('button').addEventListener('click', function (evt) {
     console.log(evt);
 })
 
-
-
-
-
 const input = document.querySelector('input');
-
-
-
-
-
 
 input.addEventListener('keydown', function () {
     console.log("KEYDOWN");
 })
+
+input.addEventListener('keyup', function () {
+    console.log('KEYUP')
+})
+
 
