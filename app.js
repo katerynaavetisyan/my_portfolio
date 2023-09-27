5039521d2d3a82657f8bfff96d6a5f6bbf5891c9
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-09-27 16:04:07 -0500

    complicated function add

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-function add(a, b) {
-    return a + b
+function add() {
+    console.log(arguments);
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    return sum;
 }
 console.log(add(15, 56, 56, 74));
+console.log(add(3, 4, 7));
