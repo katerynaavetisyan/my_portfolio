327078db71863973526712e099ce901f183e4b62
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-10-02 16:25:28 -0500

    button added with an alert

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-function add() {
-    let args = Array.from(arguments);
-    return args.reduce(function (acc, cur) {
-        return acc + cur;
-    })
-}
-
-
-
-console.log(add(56, 56);
-
-
-console.log(add(5, 2, 4))
+document.querySelector('button').addEventListener('click', function () {
+    alert("CLICK")
+})
