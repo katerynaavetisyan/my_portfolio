ff13c406f458c1b2c96cca7ab30e1b8f8ff9f34b
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-10-04 10:01:03 -0500

    function addNumbers

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 
-document.querySelector('button').addEventListener('click',
-    function (evt) {
-        console.log(evt);
+// document.querySelector('button').addEventListener('click',
+//     function (evt) {
+//         console.log(evt);
 
-    })
-const input = document.querySelector('input');
-input.addEventListener('keydown', function () {
-    console.log("KEYDOWN");
+//     })
+// const input = document.querySelector('input');
+// input.addEventListener('keydown', function () {
+//     console.log("KEYDOWN");
+// })
+
+function addNumbers(a, b {
+    return a + b; 
 })
