e03d6d6133476e6608296f06891e527bc9a5b14d
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-10-06 14:57:38 -0500

    keydown

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 
-// document.querySelector('button').addEventListener('click',
-//     function (evt) {
-//         console.log(evt);
+document.querySelector('button').addEventListener('click',
+    function (evt) {
+        console.log(evt);
 
-//     })
-// const input = document.querySelector('input');
-// input.addEventListener('keydown', function () {
-//     console.log("KEYDOWN");
-// })
+    })
+const input = document.querySelector('input');
+input.addEventListener('keydown', function () {
+    console.log("KEYDOWN");
+})
 
-let i = 1,
-while (i < 100) {
-
-    document.write(i + ", ";)
-}
