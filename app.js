adf34061cc06d81e6389571142e417069db8f77a
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-10-16 17:18:20 -0500

    learn

--- app.js
+++ app.js
@@ -15,21 +15,21 @@ input.addEventListener('keyup', function () {
     console.log("KEYUP");
 })
 
-window.addEventListener('keydown', function (e) {
-    switch (e.code) {
-        case 'ArrowUp':
-            console.log("UP");
-            break;
-        case 'ArrowDown':
-            console.log("DOWN");
-            break;
-        case 'ArrowLeft':
-            console.log("LEFT");
-            break;
-        case 'ArrowRight':
-            console.log("RIGHT");
-            break;
-        default:
-            console.log("IGNORED");
-    }
-})
+// window.addEventListener('keydown', function (e) {
+//     switch (e.code) {
+//         case 'ArrowUp':
+//             console.log("UP");
+//             break;
+//         case 'ArrowDown':
+//             console.log("DOWN");
+//             break;
+//         case 'ArrowLeft':
+//             console.log("LEFT");
+//             break;
+//         case 'ArrowRight':
+//             console.log("RIGHT");
+//             break;
+//         default:
+//             console.log("IGNORED");
+//     }
+// })
