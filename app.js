1c725cf80b04453c8cf35b53193a486661b97e79
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-10-26 10:10:08 -0500

    the loop jump

--- app.js
+++ app.js
@@ -1,37 +1,8 @@
+for (i = 0; i < 10; i++) {
+    if (i == 5) {
 
-document.querySelector('button').addEventListener('click',
-    function (evt) {
-        console.log(evt);
-
-    })
-
-
-
-const input = document.querySelector('input');
-input.addEventListener('keydown', function (e) {
-    console.log(e.key);
-    console.log(e.code);
-})
-
-input.addEventListener('keyup', function () {
-    console.log("KEYUP");
-})
-
-window.addEventListener('keydown', function (e) {
-    switch (e.code) {
-        case 'ArrowUp':
-            console.log("UP");
-            break;
-        case 'ArrowDown':
-            console.log("DOWN");
-            break;
-        case 'ArrowLeft':
-            console.log("LEFT");
-            break;
-        case 'ArrowRight':
-            console.log("RIGHT");
-            break;
-        default:
-            console.log("IGNORED");
+        continue
+        ;
     }
-})
+    console.log(i);
+}
