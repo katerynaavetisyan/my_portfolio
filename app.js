b8670c2d1d1716133c0fbefb078346124dbedf72
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-10-30 14:34:34 -0500

    Form Events and Prevent Default

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-document.querySelector('button').addEventListener('click', function (evt) {
-    console.log(evt);
-})
-
-// const input = document.querySelector('input');
-// input.addEventListener('keydown', function (e) {
-//     console.log(e.key)
-//     console.log(e.code);
-// })
-
-window.addEventListener('keydown', function (e) {
-    switch (e.code) {
-        case 'ArrowUp':
-            console.log("UP!");
-            break;
-        case 'ArrowDown':
-            console.log("DOWN!");
-            break;
-        case 'ArrowLeft':
-            console.log("LEFT!");
-            break;
-        case 'ArrowRight':
-            console.log("RIGHT!")
-            break;
-        default:
-            console.log("IGNORED!");
-    }
+const form = document.querySelector("#shelterForm");
+const input = document.querySelector("#catName");
+const list = document.querySelector("#cats");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const catName = input.value;
+    const newLi = document.createElement("Li");
+    newLi.innerText = catName;
+    list.append(newLi);
+    input.value = "";
 })
