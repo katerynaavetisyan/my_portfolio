ff66931d0a58a1d2b757fad221ac613febcbda9c
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-11-01 12:13:55 -0500

    change event

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
-const tweetForm = document.querySelector('#tweetForm');
-const tweetContainer = document.querySelector('#tweets');
-tweetForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    // const usernameInput = document.querySelectorAll('input')[0];
-    // const tweetInput = document.querySelectorAll('input')[1];
-    const usernameInput = tweetForm.elements.username;
-    const tweetInput = tweetForm.elements.tweet;
-    addTweet(usernameInput.value, tweetInput.value);
-    usernameInput.value = '';
-    tweetInput.value = '';
+const input = document.querySelector('input');
 
+input.addEventListener('change', function (e) {
+    console.log("Test");
 })
-
-const addTweet = (username, tweet) => {
-    const newTweet = document.createElement('li');
-    const bTag = document.createElement('b');
-    bTag.append(username)
-    newTweet.append(bTag);
-    newTweet.append(`- ${tweet}`)
-    console.log(newTweet);
-    // console.log(usernameInput.value, tweetInput.value);
-    tweetContainer.append(newTweet).text
-}
