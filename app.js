aa06564665518b32e52b041b787dd546c9b55618
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-11-06 08:37:34 -0600

    stopPropagation

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const button = document.querySelector('#changeColor');
 const container = document.querySelector('#container');
 
+
 button.addEventListener('click', function (e) {
     e.stopPropagation();
     container.style.backgroundColor = makeRandColor();
 })
+
 container.addEventListener('click', function () {
     container.classList.toggle('hide');
 })
+
 
 const makeRandColor = () => {
     const r = Math.floor(Math.random() * 255);
