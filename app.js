523df346a6d2d173eff595a50875cd65074e6bd9
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-11-08 12:58:45 -0600

    Score Keeper 1

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-// const lis = document.querySelectorAll('li');
-// for (let li of lis) {
-//     li.addEventListener('click', function () {
-//         li.remove();
-//     })
-// }
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
 
+let p1Score = 0;
+let winningScore = 5;
+let isGameOver = false;
 
+p1Button.addEventListener('click', function () {
+    if (!isGameOver) {
+        p1Score += 1;
+        if (p1Score === winningScore) {
+            isGameOver = true;
 
-const tweetsContainer = document.querySelector('#tweets');
-tweetForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const usernameInput = tweetForm.elements.username;
-    const tweetInput = tweetForm.elements.tweet;
-    addTweet(usernameInput.value, tweetInput.value)
-    usernameInput.value = '';
-    tweetInput.value = '';
+        }
+        p1Display.textContent = p1Score;
+    }
+})
+p2Button.addEventListener('click', function () {
+    if (!isGameOver) {
+        p2Score += 1;
+        if (p2Score === winningScore) {
+            isGameOver = true;
 
-});
-const addTweet = (username, tweet) => {
-    const newTweet = document.createElement('li');
-    const bTag = document.createElement('b');
-    bTag.append(username)
-    newTweet.append(bTag);
-    newTweet.append(`- ${tweet}`)
-    tweetsContainer.append(newTweet);
-}
-
-tweetsContainer.addEventListener('click', function (e) {
-    e.target.nodeName === 'LI' && e.target.remove();
+        }
+        p2Display.textContent = p2Score;
+    }
 })
