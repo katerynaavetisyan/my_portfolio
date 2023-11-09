3165728087aaa3e65fd59de16f2ce34786e80d0d
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-11-09 14:28:37 -0600

    winning score

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
 const winningScoreSelect = document.querySelector('#playto');
 
+
 let p1Score = 0;
 let winningScore = 5;
 let isGameOver = false;
