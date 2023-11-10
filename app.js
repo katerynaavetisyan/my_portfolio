22a2cac88c29df55790409b99bcb8add0defa2d2
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-11-10 14:58:11 -0600

    magic of promises

--- app.js
+++ app.js
@@ -1,55 +1,19 @@
-const p1Button = document.querySelector('#p1Button');
-const p2Button = document.querySelector('#p2Button');
-const resetButton = document.querySelector('#reset')
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
-const winningScoreSelect = document.querySelector('#playto');
-
-
-let p1Score = 0;
-let winningScore = 5;
-let isGameOver = false;
-
-p1Button.addEventListener('click', function () {
-    if (!isGameOver) {
-        p1Score += 1;
-        if (p1Score === winningScore) {
-            isGameOver = true;
-            p1Display.classList.add('winner');
-            p2Display.classList.add('loser');
-
-        }
-        p1Display.textContent = p1Score;
-    }
-})
-p2Button.addEventListener('click', function () {
-    if (!isGameOver) {
-        p2Score += 1;
-        if (p2Score === winningScore) {
-            isGameOver = true;
-            p2Display.classList.add('winner');
-            p1Display.classList.add('loser');
-        }
-        p2Display.textContent = p2Score;
-    }
-})
-
-winningScoreSelect.addEventListener('change', function () {
-    winningScore = parseInt(this.value);
-    reset();
-})
-
-
-resetButton.addEventListener('click', reset)
-
-
-
-function reset() {
-    isGameOver = false;
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
-    p1Display.classList.remove('winner', 'loser');
-    p2Display.classList.remove('loser', 'winner');
-} 
+fakeRequestPromise('yelp.com/api/coffee/page1')
+    .then((data) => {
+        console.log("IT WORKED!!! (page1)")
+        console.log(data)
+        return fakeRequestPromise('yelp.com/api/coffee/page2')
+    })
+    .then((data) => {
+        console.log("IT WORKED!!!! (page2)")
+        console.log(data)
+        return fakeRequestPromise('yelp.com/api/coffee/page3')
+    })
+    .then((data) => {
+        console.log("IT WORKED!!!! (page3)")
+        console.log(data)
+    })
+    .catch((err) => {
+        console.log("OH NO, A REQUEST FAILED!!!")
+        console.log(err);
+    })
