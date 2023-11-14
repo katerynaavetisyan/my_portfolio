096564d7ab7da5b71887fb5b22edb22c4bd83c95
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-11-14 10:28:04 -0600

    fake request

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ fakeRequestPromise('yelp.com/api/coffee/page1')
         console.log(data)
         return fakeRequestPromise('yelp.com/api/coffee/page2')
     })
+
     .then((data) => {
         console.log("IT WORKED!!!! (page2)")
         console.log(data)
