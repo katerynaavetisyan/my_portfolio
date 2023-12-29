c4c6062ee88bc282fd6ac91d4848f5b28e6b15fe
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-12-29 13:13:38 -0600

    Added JavaScript function to the navbar

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('.navbar')
+window.onscroll = function () {
+    const top = document.documentElement.scrollTop;
+    if (top >= 100) {
+        header.classList.add("navbarDark");
+    } else {
+        header.classList.remove("navbarDark");
+    }
+};
